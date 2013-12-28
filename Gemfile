1d37c9e940acdb2a91c9fd60c0c9b87a091b6a8d
Joey Wendt <joey@joeywendt.com>
2013-12-28 12:09:35 -0600

    Add fetch user method, vcr for testing, octokit client.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
 source 'https://rubygems.org/'
 
 gem 'octokit'
-gem 'rspec'
 gem 'rake'
+
+group :test do
+  gem 'multi_json'
+  gem 'rspec'
+  gem 'vcr'
+  gem 'webmock'
+end
+
--- lib/star_charter.rb
+++ lib/star_charter.rb
@@ -22,6 +22,21 @@ class StarCharter
       result
     end
 
+    # Fetch a user and their starred repositories
+    #
+    # @param user [String] GitHub username.
+    # @return [Hash] User and their starred repositories.
+    def fetch_user(user)
+      user_hash = octokit.user(user).to_hash
+      user_hash[:starred] = octokit.starred(user)
+      user_hash
+    end
+
+    private
+
+    def octokit
+      @octokit ||= Octokit::Client.new :auto_paginate => true
+    end
   end
 end
 
