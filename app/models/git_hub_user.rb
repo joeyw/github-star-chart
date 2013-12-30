91abd81fac26eadd8d129e35966c4dd6b18a9188
Joey Wendt <joey@joeywendt.com>
2013-12-30 16:34:18 -0600

    add fetch to GitHubUser model
    a test would have told me i forgot to save my changes but i am dumb and
    a bad programmer

--- app/models/git_hub_user.rb
+++ app/models/git_hub_user.rb
@@ -1,2 +1,19 @@
 class GitHubUser
+
+  # Fetch a user and their starred repositories
+  #
+  # @param user [String] GitHub username.
+  # @return [Hash] User and their starred repositories.
+  def fetch(user)
+    user_hash = octokit.user(user).to_hash
+    user_hash[:starred] = octokit.starred(user)
+    user_hash
+  end
+
+  private
+
+  def octokit
+    @octokit ||= Octokit::Client.new :auto_paginate => true
+  end
+
 end
